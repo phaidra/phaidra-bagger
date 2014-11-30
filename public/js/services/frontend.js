7f54d61bebd65eaa82f48712e0c58b771a1c9e35
RastislavHudak <hudak.rastislav@gmail.com>
2014-11-30 15:32:46 +0100

    urls

--- public/js/services/frontend.js
+++ public/js/services/frontend.js
@@ -1,54 +1,54 @@
 angular.module('frontendService', [])
 .factory('FrontendService', function($http) {
-	
+
 	return {
-		
+
 		updateSelection: function(selection){
 			return $http({
 				method  : 'POST',
-				url     : $('head base').attr('href')+'selection',
+				url     : $('head base').attr('href')+'/selection',
 				data    : { selection: selection }
-			});	        
+			});
 		},
-		
+
 		getSelection: function(selection){
 			return $http({
 				method  : 'GET',
-				url     : $('head base').attr('href')+'selection'
-			});	        
+				url     : $('head base').attr('href')+'/selection'
+			});
 		},
-		
+
 		loadSettings: function(){
 			return $http({
 				method  : 'GET',
-				url     : $('head base').attr('href')+'settings/my'
-			});	        
+				url     : $('head base').attr('href')+'/settings/my'
+			});
 		},
-		
+
 		saveSettings: function(type, settings){
 			var data = {};
-			data[type+'_settings'] = settings;			
+			data[type+'_settings'] = settings;
 			return $http({
 				method  : 'POST',
-				url     : $('head base').attr('href')+'settings/',
+				url     : $('head base').attr('href')+'/settings/',
 				data    : data
-			});	        
+			});
 		},
-		
+
 		toggleClassification: function(uri){
 			return $http({
 				method  : 'POST',
-				url     : $('head base').attr('href')+'classifications',
+				url     : $('head base').attr('href')+'/classifications',
 				data    : { uri: uri }
-			});	        
+			});
 		},
 
 		getClassifications: function(terms){
 			return $http({
 				method  : 'GET',
-				url     : $('head base').attr('href')+'classifications'
-			});	        
-		}	
+				url     : $('head base').attr('href')+'/classifications'
+			});
+		}
 
 	}
 });
