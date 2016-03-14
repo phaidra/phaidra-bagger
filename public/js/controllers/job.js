ae35af8dc36081d14a32b15de94f52408f2b5a18
RastislavHudak <hudak.rastislav@gmail.com>
2016-03-14 18:05:20 +0100

    bag selection on job view

--- public/js/controllers/job.js
+++ public/js/controllers/job.js
@@ -5,6 +5,8 @@ app.controller('JobCtrl',  function($scope, $modal, $location, DirectoryService,
 	$scope.alerts = [];	
 	$scope.items = [];
 	$scope.jobid = '';      
+
+	$scope.selection = [];
 	
 	$scope.totalItems = 0;
     $scope.currentPage = 1;
@@ -28,7 +30,45 @@ app.controller('JobCtrl',  function($scope, $modal, $location, DirectoryService,
 		$scope.jobid = $scope.initdata.jobid;
 		$scope.refreshResults();
     };
-    
+
+    $scope.selectNone = function(event){
+        $scope.selection = [];
+    };
+
+    $scope.selectVisible = function(event){
+        $scope.selection = [];
+        for( var i = 0 ; i < $scope.items.length ; i++ ){
+            $scope.selection.push($scope.items[i].bagid);
+        }
+    };
+
+    $scope.selectAll = function(event){   
+        var promise = BagService.searchJobBags($scope.jobid, 0, 0, $scope.sortfield, $scope.sortvalue);
+	    $scope.loadingTracker.addPromise(promise);
+	    promise.then(
+	      	function(response) { 
+	      		$scope.alerts = response.data.alerts;
+	      		$scope.selection = [];
+                for( var i = 0 ; i < response.data.items.length ; i++ ){
+                    $scope.selection.push(response.data.items[i].bagid);
+                }	      		
+	      	}
+	      	,function(response) {
+	      		$scope.alerts = response.data.alerts;
+	      		$scope.alerts.unshift({type: 'danger', msg: "Error code "+response.status});
+	      	}
+	     );    	      
+    }
+
+    $scope.toggleBag = function(bagid) {
+        var idx = $scope.selection.indexOf(bagid);
+        if(idx == -1){
+                $scope.selection.push(bagid);
+        }else{
+                $scope.selection.splice(idx,1);
+        }
+    };
+
     $scope.refreshResults = function() {
     	$scope.searchJobBags($scope.jobid, $scope.from, $scope.limit, $scope.sortfield, $scope.sortvalue); 
     }
